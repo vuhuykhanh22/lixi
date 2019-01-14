647e4a022ad349b4fe8f9313831ef402a17e59f9
Khanh Vu <vuhuykhanh22@github.com>
2019-01-15 00:27:59 +0700

    Updates

---
+++ precache-manifest.71d7e10ba8daf63819fa2d1d2e30711e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "39cbd09868cd4214b388c521e0287142",
+    "url": "/lixi/static/media/newyear-background.39cbd098.jpg"
+  },
+  {
+    "revision": "741c67c23bec0087e7ad",
+    "url": "/lixi/static/css/main.52f16185.chunk.css"
+  },
+  {
+    "revision": "aaf542d004e6143833bc9a01098c36f1",
+    "url": "/lixi/static/media/happy-4.aaf542d0.gif"
+  },
+  {
+    "revision": "ab305a38c83fc0307ec6",
+    "url": "/lixi/static/js/1.ab305a38.chunk.js"
+  },
+  {
+    "revision": "c2592817303b5432a8a5",
+    "url": "/lixi/static/js/runtime~main.c2592817.js"
+  },
+  {
+    "revision": "e6ae4033d9c15bfec971771f239025e6",
+    "url": "/lixi/static/media/firework.e6ae4033.gif"
+  },
+  {
+    "revision": "741c67c23bec0087e7ad",
+    "url": "/lixi/static/js/main.741c67c2.chunk.js"
+  },
+  {
+    "revision": "331d8457118c2fe652e3819aa5309830",
+    "url": "/lixi/static/media/happy-1.331d8457.gif"
+  },
+  {
+    "revision": "7c5dccae829b2b572850ad24fe335118",
+    "url": "/lixi/static/media/happy-2.7c5dccae.gif"
+  },
+  {
+    "revision": "a907eab5e96994d77c7c240b086ecbb2",
+    "url": "/lixi/static/media/happy-3.a907eab5.gif"
+  },
+  {
+    "revision": "ab305a38c83fc0307ec6",
+    "url": "/lixi/static/css/1.361302d1.chunk.css"
+  },
+  {
+    "revision": "752bbff1470fda03afed6e6208b9906c",
+    "url": "/lixi/index.html"
+  }
+];
